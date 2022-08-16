219416b54188d0efed71d54db3cde348d54f4b6a
Nghia Dang <vannghiadang79@gmail.com>
2022-08-16 14:23:20 +0700

    setup & nav

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// MENU
+const navItems = document.querySelectorAll('.nav__item');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navItems.forEach(item => {
+            item.classList.remove('active-item');
+        });
+        this.parentNode.classList.add('active-item');
+    });
+});
+
+const menu = document.getElementById('main-nav');
+const main = document.getElementById('main');
+const toggleMenuBtn = document.getElementById('toggle-menu-btn');
+
+toggleMenuBtn.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+    main.classList.toggle('show-menu');
+});
